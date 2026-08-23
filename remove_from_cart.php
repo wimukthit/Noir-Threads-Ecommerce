acaa18be3ec6da880b35b20a396e0f3d9c553fac
Wimukthi Weerasinghe <wimukthitharuka144@gmail.com>
2026-08-23 21:59:12 +0530

    5

--- remove_from_cart.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config/db.php';
-
-if (isset($_GET['key']) && isset($_SESSION['cart'][$_GET['key']])) {
-    unset($_SESSION['cart'][$_GET['key']]);
-}
-
-header("Location: cart.php");
-exit;
